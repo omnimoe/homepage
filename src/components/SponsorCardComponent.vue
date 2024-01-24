36caa740999bd7ebb7c1baa99fbed87b995a3cca
lyuiowo <lyuiowo@icloud.com>
2024-01-24 19:07:44 +0800

    Update NotificationView.vue

---
+++ src/components/SponsorCardComponent.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+import { defineProps } from 'vue'
+</script>
+
+<template>
+  <div class="space-y-1">
+    <div class="group relative h-20 overflow-hidden rounded-lg bg-white">
+      <a href="">
+        <div class="flex flex-row p-3">
+          <img src="../assets/images/logo.png.webp" alt="ad" class="w-[55px] h-[55px]">
+          <div class="ml-3">
+            <div class="font-medium tracking-wider md:text-sm lg:w-[135px] lg:truncate lg:whitespace-nowrap lg:text-base">
+              微信扫码赞助本站
+            </div>
+            <div class="hidden lg:block">
+              <div class="group-hover:hidden">
+                <div class="mt-1.5 mb-1 text-xs text-gray-500">
+                  <span>服务器还剩<strong class="mx-1">300</strong>天</span>
+                  <div class="relative left-0.5 bottom-1 inline-flex w-fit">
+                    <span class="text-xs text-blue-500 font-medium"><span class="mr-[0.5px]">+1</span>年</span>
+                  </div>
+                </div>
+                <div class="flex h-1 w-full overflow-hidden rounded-full bg-gray-200">
+                  <div class="flex flex-col justify-center overflow-hidden bg-blue-500" style="width: 5%;"></div>
+                </div>
+              </div>
+              <div class="hidden group-hover:block">
+                <div class="mt-1.5 mb-1 text-xs text-gray-500">
+                  <span>距离下个目标还差<strong class="mx-1">95%</strong></span>
+                </div>
+                <div class="flex h-1 w-full overflow-hidden rounded-full bg-gray-200">
+                  <div class="flex flex-col justify-center overflow-hidden bg-blue-500" style="width: 5%;"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/NotificationView.vue
+++ src/views/NotificationView.vue
@@ -2,6 +2,7 @@
 
 import TitleComponent from '@/components/TitleComponent.vue'
 import UserCardComponent from '@/components/UserCardComponent.vue'
+import SponsorCardComponent from '@/components/SponsorCardComponent.vue'
 
 const stats = true;
 </script>
@@ -12,12 +13,30 @@ const stats = true;
       <div class="relative w-0 shrink grow lg:w-9/12 lg:grow-0">
         <div class="h-screen divide-y divide-gray-100">
           <title-component title="消息中心" />
+          <div class="mt-2 bg-white px-6 pt-3 md:rounded-lg">
+            <div class="border-b border-gray-200">
+              <nav class="-mb-0.5 flex space-x-6">
+                <div class="inline-flex cursor-pointer border-b-2 border-blue-500 py-2 px-1 text-sm text-gray-500 hover:text-blue-600 border-transparent items-center gap-1 whitespace-nowrap md:text-base !border-blue-500 !text-blue-500">
+                  开源项目
+                </div>
+                <div class="inline-flex cursor-pointer border-b-2 border-blue-500 py-2 px-1 text-sm text-gray-500 hover:text-blue-600 border-transparent items-center gap-1 whitespace-nowrap md:text-base">
+                  系统消息
+                </div>
+              </nav>
+            </div>
+            <div class="mt-4 text-center text-xl">
+              <div class="py-14 text-gray-300">
+                暂无消息
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="relative hidden w-3/12 shrink-0 md:block md:grow-0">
         <div class="relative mt-2 ml-3 max-w-[244px]">
           <div class="space-y-2">
             <user-card-component :stats="stats" />
+            <sponsor-card-component />
           </div>
         </div>
       </div>
